171e7b07d246f8efaa55be7b4b157bd5d5b8b1fa
Alexander I. Zaytsev <hazzik@gmail.com>
2014-05-14 01:21:45 +1200

    fixup! Fix "Number" issue

--- src/Rhino/Utils/NumberUtil.cs
+++ src/Rhino/Utils/NumberUtil.cs
@@ -2,21 +2,21 @@
 
 namespace Rhino
 {
-    public static class NumberUtil
-    {
-        public static bool IsNumber(this object value)
-        {
-            return value is SByte ||
-                   value is Byte ||
-                   value is Int16 ||
-                   value is UInt16 ||
-                   value is Int32 ||
-                   value is UInt32 ||
-                   value is Int64 ||
-                   value is UInt64 ||
-                   value is Single ||
-                   value is Double ||
-                   value is Decimal;
-        }
-    }
+	public static class NumberUtil
+	{
+		public static bool IsNumber(this object value)
+		{
+			return value is SByte ||
+				   value is Byte ||
+				   value is Int16 ||
+				   value is UInt16 ||
+				   value is Int32 ||
+				   value is UInt32 ||
+				   value is Int64 ||
+				   value is UInt64 ||
+				   value is Single ||
+				   value is Double ||
+				   value is Decimal;
+		}
+	}
 }
